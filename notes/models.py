39ff6be619b2977650e7d5fce5bc989ea994443c
patil-vaishnavii <vaishnaviipatil2212@gmail.com>
2026-08-01 21:08:15 +0530

    created models and added create function

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Note(models.Model):
+  user = models.ForeignKey(
+    User,
+    on_delete=models.CASCADE
+  )
 
+  title = models.CharField(max_length=200)
+  content = models.TextField()
+
+  def __str__(self):
+    return self.title
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_list_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Note
 
 @login_required
 def home(request):
-  return render(request,"notes/index.html")
+  notes = Note.objects.filter(user=request.user) # to make app user-specific.
+
+  return render(
+    request,
+    "notes/index.html",
+    {"notes":notes}
+  )
+
+@login_required
+def create_note(request):
+
+  if request.method == "POST":
+    title = request.POST["title"]
+    content = request.POST["content"]
+
+    Note.objects.create(
+      user=request.user,
+      title=title,
+      content=content
+    )
+
+    return redirect("home")
+
+  return render(request,"notes/create_note.html")
